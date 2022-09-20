c96bdbb27e7a0f3f34c03666e1795034108803a4
abba_ye38 <abba_ye38@STHQ0122-06>
2022-09-20 19:32:20 +0400

    Add project files.

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BitmapImage Image;
+
+        public BitmapImage Img
+        {
+            get { return Image; }
+            set { Image = value; }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            FileInfo fileInfo = new FileInfo("cfk4sqzk966f2i2edzps.jpg");
+            Img = new BitmapImage(new Uri(fileInfo.FullName));
+        }
+    }
+}
